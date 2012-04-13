e003492dfa2774bd980c5ec0c960bd06c1dd186b
Andrew Rohm <andrew.rohm@duke.edu>
2012-04-12 22:27:34 -0400

    more feedback

--- ThreadedMatrixMultiply.cu
+++ ThreadedMatrixMultiply.cu
@@ -8,7 +8,7 @@ typedef struct{
 	int* elements;
 } Matrix;
 
-__global__ void matrixProduct(Matrix, Matrix, Matrix);
+__global__ void matrixProduct(Matrix, Matrix, Matrix, Matrix, Matrix);
 
 int matrixProduct(Matrix, Matrix, int, int);
 void printMatrix(Matrix, char[]);
@@ -17,9 +17,13 @@ main()
 {
 	//Declare vars, constants
 	int const MatSize=3;
-	Matrix Matrix1, Matrix2, Result, Res_Check;
-	Matrix dev_Matrix1, dev_Matrix2, dev_Result;
+	Matrix Matrix1, Matrix2, Result, Res_Check, BlockRow, BlockCol;
+	Matrix dev_Matrix1, dev_Matrix2, dev_Result, dev_BlockRow, dev_BlockCol;
 	
+	//Code to aid in debugging the non-determinism
+	BlockRow.height = MatSize; BlockRow.width = MatSize;
+	BlockCol.height = MatSize; BlockCol.width = MatSize;
+
 	Matrix1.width = MatSize; Matrix1.height = MatSize;
 	Matrix2.width = MatSize; Matrix2.height = MatSize;
 	Result.width = MatSize; Result.height = MatSize;
@@ -36,6 +40,9 @@ main()
 	Matrix2.elements = (int*) malloc(MemSize);
 	Result.elements = (int*) malloc(MemSize);
 	Res_Check.elements = (int*) malloc(MemSize);
+	
+	BlockRow.elements = (int*) malloc(MemSize);
+	BlockCol.elements = (int*) malloc(MemSize);
 	
 	//Initialize matrices with random values
 	for(i=1;i<=MatSize;i++)
@@ -62,10 +69,18 @@ main()
 	dev_Result.height = Result.height; dev_Result.width = Result.width;
 	cudaMalloc((void**)&dev_Result.elements,MemSize);
 	
+	dev_BlockRow.height = BlockRow.height; dev_BlockRow.width = BlockRow.width;
+	dev_BlockCol.height = BlockCol.height; dev_BlockCol.width = BlockCol.width;
+	cudaMalloc((void**)&dev_BlockRow.elements,MemSize);
+	cudaMalloc((void**)&dev_BlockCol.elements,MemSize);
+	cudaMemcpy(dev_BlockRow.elements, BlockRow.elements, MemSize, cudaMemcpyHostToDevice);
+	cudaMemcpy(dev_BlockCol.elements, BlockCol.elements, MemSize, cudaMemcpyHostToDevice);
 		
 	//Kernel Declaration
-	matrixProduct<<<blockSize,gridSize>>>(dev_Matrix1, dev_Matrix2, dev_Result);
+	matrixProduct<<<blockSize,gridSize>>>(dev_Matrix1, dev_Matrix2, dev_Result, dev_BlockRow, dev_BlockCol);
 	cudaMemcpy(Result.elements, dev_Result.elements, MemSize, cudaMemcpyDeviceToHost);
+	cudaMemcpy(BlockRow.elements, dev_BlockRow.elements, MemSize, cudaMemcpyDeviceToHost);
+	cudaMemcpy(BlockCol.elements, dev_BlockCol.elements, MemSize, cudaMemcpyDeviceToHost);
 	
 	gettimeofday(&end,NULL);
 	printf("End Values %ld, %ld\n",end.tv_sec,end.tv_usec);
@@ -73,6 +88,10 @@ main()
 	printMatrix(Matrix1,"Matrix 1\n");
 	printMatrix(Matrix2, "Matrix 2\n");
 	printMatrix(Result, "Result Matrix\n");
+	printMatrix(BlockRow, "Compute Row Used\n");
+	printMatrix(BlockCol, "Compute Column Used\n");
+	
+	
 	//Compute time elapsed
 	
 	elapsed.tv_sec = (end.tv_sec-start.tv_sec);
@@ -129,15 +148,17 @@ void printMatrix(Matrix Mat, char name[])
 	return;
 }
 
-__global__ void matrixProduct(Matrix Mat1, Matrix Mat2, Matrix Res)
+__global__ void matrixProduct(Matrix Mat1, Matrix Mat2, Matrix Res, Matrix bkRow, Matrix bkCol)
 {
 	int row = blockIdx.x;
 	int col = blockIdx.y;
 	int k,sum;
 	sum=0;
-	//for(k=1;k<=Mat1.width;k++)
-	//{
-		sum=(Mat1.elements[(row*Mat1.width)+1])*(Mat2.elements[(1*Mat2.width)+col])+(Mat1.elements[(row*Mat1.width)+2])*(Mat2.elements[(2*Mat2.width)+col])+(Mat1.elements[(row*Mat1.width)+3])*(Mat2.elements[(3*Mat2.width)+col]);
-	//}
+	for(k=1;k<=Mat1.width;k++)
+	{
+		sum=sum+(Mat1.elements[(row*Mat1.width)+k])*(Mat2.elements[(k*Mat2.width)+col]);
+	}
+	bkRow.elements[(row*bkRow.width)+col]=row;
+	bkCol.elements[(row*bkRow.width)+col]=col;
 	Res.elements[(row*Res.width)+col]=sum;
 }
